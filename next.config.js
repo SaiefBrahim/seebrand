2e4b83fce4ffa1d4c0b5b5bc101dfcb445b23696
saiefbrahim <seefbrahim@gmail.com>
2022-11-27 17:25:20 +0100

    fixing last issues after lighthouse debug last

--- next.config.js
+++ next.config.js
@@ -9,20 +9,28 @@ const securityHeaders = [
     value: "1; mode=block",
   },
   {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
-  },
-  {
     key: "X-Frame-Options",
     value: "SAMEORIGIN",
   },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
 ];
 const nextConfig = {
+  poweredByHeader: false,
   reactStrictMode: true,
   async headers() {
     return [
       {
-        // Apply these headers to all routes in the application.
         source: "/:path*",
         headers: securityHeaders,
       },
