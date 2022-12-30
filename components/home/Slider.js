6895959246831d8915eb6a931a485d731743ba51
saiefbrahim <seefbrahim@gmail.com>
2022-12-30 12:32:36 +0100

    updating home page

--- components/home/Slider.js
+++ components/home/Slider.js
@@ -1,16 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useState, useEffect } from "react";
+import coverPic1 from "../../public/coverpic1.webp";
+import coverPic2 from "../../public/coverpic2.webp";
+import coverPic3 from "../../public/coverpic3.webp";
+import mobileSlide1 from "../../public/mobileslide1.webp";
+import mobileSlide2 from "../../public/mobileslide2.webp";
+import mobileSlide3 from "../../public/mobileslide3.webp";
+import shopNowButton from "../../public/shopnowbutton.webp";
+import rightArrow from "../../public/rightarrow.webp";
+import leftArrow from "../../public/leftarrow.webp";
 
 const Slider = () => {
   const [bgImage, setBgImage] = useState(0);
   const [bgMobile, setBgMobile] = useState(0);
 
-  const slides = ["coverpic1.webp", "coverpic2.webp", "coverpic3.webp"];
-  const mobileSlides = [
-    "mobileslide1.webp",
-    "mobileslide2.webp",
-    "mobileslide3.webp",
-  ];
+  const slides = [coverPic1, coverPic2, coverPic3];
+  const mobileSlides = [mobileSlide1, mobileSlide2, mobileSlide3];
   const nextSlideHandler = () => {
     bgImage >= slides.length - 1 ? setBgImage(0) : setBgImage(bgImage + 1);
   };
@@ -29,7 +35,6 @@ const Slider = () => {
       : setBgMobile(bgMobile - 1);
   };
   useEffect(() => {
-    var i = 0;
     const sliderHandler = setInterval(() => {
       nextSlideHandler();
       nextMobileSlideHandle();
@@ -39,35 +44,32 @@ const Slider = () => {
 
   return (
     <>
-      <div
-        className="hidden lg:block bg-no-repeat h-screen relative"
-        style={{
-          backgroundImage: `url(${`/${slides[bgImage]}`})`,
-          backgroundSize: "100%",
-          objectFit: "cover",
-          transition: "background-image 0.3s linear",
-          WebkitTransition: "background-image 0.3s linear",
-        }}
-      >
-        <div className="absolute flex flex-col top-[20%] left-[16%] gap-8 ">
+      <div className="hidden lg:block relative">
+        <Image
+          className=" transition-all duration-300 ease-linear pointer-events-none"
+          src={slides[bgImage]}
+          alt="Slide Show Images"
+          loading="eager"
+        />
+        <div className="absolute top-[20%] left-[16%] flex flex-col gap-8">
           <p className=" cursor-default text-white w-60 leading-snug text-5xl font-bold">
             Sale of the summer collection
           </p>
-          <button
-            aria-label="Shop Now"
-            className=" flex items-center gap-3 cursor-pointer w-fit"
-          >
-            <Image
-              className=" w-10"
-              src="/shopnowbutton.webp"
-              width={156}
-              height={156}
-              alt="Shop Now"
-            />
-            <p className=" text-white font-medium text-base">SHOP NOW</p>
-          </button>
+          <Link href="#">
+            <button
+              aria-label="Shop Now"
+              className="flex items-center gap-3 hover:-translate-y-1 hover:scale-110 transition-all duration-300 ease-linear delay-75"
+            >
+              <Image
+                className=" w-10 h-10 pointer-events-none"
+                src={shopNowButton}
+                alt="Shop Now"
+              />
+              <p className="text-white font-medium">SHOP NOW</p>
+            </button>
+          </Link>
         </div>
-        <div className=" absolute flex justify-end right-20 bottom-10">
+        <div className="absolute flex justify-end right-20 bottom-10">
           <button
             aria-label="Previous Slider"
             className=" w-12 btn btn-primary p-3 bg-gray-700 border-none bg-opacity-20 rounded-none"
@@ -75,12 +77,7 @@ const Slider = () => {
               prevSlideHandler();
             }}
           >
-            <Image
-              src="/leftarrow.webp"
-              width={100}
-              height={87}
-              alt="Back button"
-            />
+            <Image src={leftArrow} alt="Back button" />
           </button>
           <button
             aria-label="Next Slider"
@@ -89,12 +86,7 @@ const Slider = () => {
               nextSlideHandler();
             }}
           >
-            <Image
-              src="/rightarrow.webp"
-              width={100}
-              height={87}
-              alt="Next button"
-            />
+            <Image src={rightArrow} alt="Next button" />
           </button>
         </div>
         <div className="flex justify-around w-[70%] rounded-tr-full absolute bottom-0 left-0 p-7 bg-white">
@@ -153,32 +145,26 @@ const Slider = () => {
       {/* mobile slider */}
 
       <div className=" lg:hidden">
-        <div
-          className="h-[500px] bg-no-repeat relative"
-          style={{
-            backgroundImage: `url(${`/${mobileSlides[bgMobile]}`})`,
-            backgroundSize: "100%",
-            transition: "background-image 0.3s linear",
-            WebkitTransition: "background-image 0.3s linear",
-          }}
-        >
-          <div className="flex flex-col pt-[65%] ml-[10%] gap-9 lg:hidden ">
+        <div className="h-[500px] relative">
+          <Image
+            className=" transition-all ease-linear duration-1000 pointer-events-none"
+            src={mobileSlides[bgMobile]}
+            alt="Slide Show Images"
+            loading="eager"
+          />
+          <div className="absolute bottom-14 left-8 flex flex-col gap-6">
             <p className=" cursor-default text-white w-56 text-4xl font-bold">
               Sale of the summer collection
             </p>
-            <button
-              aria-label="Shop Now"
-              className=" flex items-center gap-3 cursor-pointer w-fit"
-            >
-              <Image
-                className=" w-9"
-                src="/shopnowbutton.webp"
-                width={156}
-                height={156}
-                alt="Shop Now"
-              />
-              <p className=" text-white font-medium">SHOP NOW</p>
-            </button>
+            <Link href="#">
+              <button
+                aria-label="Shop Now"
+                className=" flex items-center gap-3 pointer-events-none"
+              >
+                <Image className=" w-9" src={shopNowButton} alt="Shop Now" />
+                <p className=" text-white font-medium">SHOP NOW</p>
+              </button>
+            </Link>
           </div>
           <div className=" absolute bottom-14 right-10">
             <button
@@ -188,12 +174,7 @@ const Slider = () => {
                 prevMobileSlideHandle();
               }}
             >
-              <Image
-                src="/leftarrow.webp"
-                width={100}
-                height={87}
-                alt="Back button"
-              />
+              <Image src={leftArrow} alt="Back button" />
             </button>
             <button
               aria-label="Next Slider"
@@ -202,12 +183,7 @@ const Slider = () => {
                 nextMobileSlideHandle();
               }}
             >
-              <Image
-                src="/rightarrow.webp"
-                width={100}
-                height={87}
-                alt="Next button"
-              />
+              <Image src={rightArrow} alt="Next button" />
             </button>
           </div>
         </div>
